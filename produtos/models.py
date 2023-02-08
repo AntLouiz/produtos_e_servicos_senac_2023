94b098cb3e356ae8fa2d97f7bcfa131c3e5f420f
Luiz Gonzaga <luizrodrigogogo@gmail.com>
2023-02-08 20:46:49 -0300

    Aula 08-02-2023
    - Adiciona o vendedor no contexto
    - Adiciona model de produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.FloatField(default=0)
+    estoque = models.IntegerField(default=0)
--- produtos/views.py
+++ produtos/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
 def listagem_produtos(request):
-    context = {
+    produtos_dos_vendedores = [{
+        'vendedor': {'nome': 'John Doe'},
         'produtos': [
             {'nome': 'Uva', 'preco': 1}, 
             {'nome': 'Melancia', 'preco': 10},
             {'nome': 'Banana', 'preco': 14}
         ]
-    }
+    },
+    {
+        'vendedor': {'nome': 'Marta Doe'},
+        'produtos': [
+            {'nome': 'Maçã', 'preco': 5}, 
+        ]
+    }]
+    context = {'produtos_dos_vendedores': produtos_dos_vendedores }
     return render(request, 'templates/listagem_produtos.html', context)
