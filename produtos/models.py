0d5d896d5c3e798c47a6fa3741d9ac569961d593
Luiz Gonzaga <luizrodrigogogo@gmail.com>
2023-02-15 20:51:47 -0300

    Adiciona o tipo no model Produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+TIPO_PRODUTO = 'P'
+TIPO_SERVICO = 'S'
+
 class Produto(models.Model):
+    TIPOS_CHOICES = [
+        (TIPO_PRODUTO, 'Produto'),
+        (TIPO_SERVICO, 'Serviço')
+    ]
+
     nome = models.CharField(max_length=100)
     preco = models.FloatField(default=0)
     estoque = models.IntegerField(default=0)
+    tipo = models.CharField(max_length=2, choices=TIPOS_CHOICES, default=TIPO_PRODUTO)
     excluido = models.BooleanField(default=False)
